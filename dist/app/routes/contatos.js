b3c00163a79ad2c4ff98f2ac7a5e13400d341b51
=Luis <=luisw.h@live.com>
2021-04-19 00:31:11 -0300

    commit teste

--- dist/app/routes/contatos.js
+++
@@ -1,8 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
-var _ContatosController = require('../controllers/ContatosController'); var _ContatosController2 = _interopRequireDefault(_ContatosController);
-
-const routes = new (0, _express.Router)();
-
-routes.post('/contatos', _ContatosController2.default.store);
-
-exports. default = routes;
--- dist/swagger.js
+++
@@ -1,9 +0,0 @@
-"use strict";const swaggerAutogen = require('swagger-autogen')();
-const config = require('./config/swagger');
-
-const endpoints = [
- 
-];
-const outputFile = './src/swagger_documentation.json';
-
-swaggerAutogen(outputFile, endpoints, config);
