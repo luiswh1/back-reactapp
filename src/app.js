ec43c1c615b60303cab9ff0e6fdedc6c0ac74c13
=Luis <=luisw.h@live.com>
2021-04-19 00:49:47 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import './database/index';
 import ComentarioRout from './app/routes/comentario';
 import ContatoRout from './app/routes/contatos';
+import cors from 'cors';
 
 
 class App {
@@ -15,7 +16,7 @@ class App {
 
     config() {
         this.server.use(express.json());
-        // this.server.use(cors());
+        this.server.use(cors());
         dotenv.config({
             path: process.env.NODE_ENV === 'test'
             ? '.env.test' : '.env',
